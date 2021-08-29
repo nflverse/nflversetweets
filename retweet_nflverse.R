452f00a0539270da8cc74deb6390d0cd80a12170
mrcaseb <roemerle@gmail.com>
2021-08-29 17:04:16 +0200

    update retweet script

--- retweet_nflverse.R
+++ retweet_nflverse.R
@@ -1,45 +1,51 @@
 library(rtweet)
-library(dplyr)
-library(purrr)
+library(dplyr, warn.conflicts = FALSE)
+library(purrr, warn.conflicts = FALSE)
+library(tidyr, warn.conflicts = FALSE)
 
 #' search_tweets() FOR #nflverse WHERE status_id > status_from_last_run EVERY fifteen minutes
 #'
 #' apply cleaning:
 #' a) do not retweet where more than 3 hashtags are used
-#' b) do not retweet more than one message from the same author
+#' b) do not retweet more than one message from the same author (author vatiable is missing?)
 #' c) remove swearing and/or not-pg tweets? (censor it)? (FUTURE)
 
-# TODO figure out how to auth as nflversetweets bot
-# TODO GHA
+api_key <- Sys.getenv("TWITTERAPIKEY")
+api_secret <- Sys.getenv("TWITTERAPISECRET")
+access_token <- Sys.getenv("TWITTERACCESSTOKEN")
+access_secret <- Sys.getenv("TWITTERACCESSTOKENSECRET")
 
-twitter_token <- rtweet::create_token(
-  app = "Tan-DataCollection",
-  consumer_key = Sys.getenv("TWITTERAPIKEY"),
-  consumer_secret = Sys.getenv("TWITTERAPISECRET"),
-  access_token = Sys.getenv("TWITTERACCESSTOKEN"),
-  access_secret = Sys.getenv("TWITTERACCESSTOKENSECRET")
+bot <- rtweet::rtweet_bot(
+  api_key = api_key,
+  api_secret = api_secret,
+  access_token = access_token,
+  access_secret = access_secret
 )
+
+rtweet::auth_as(bot)
 
 last_id_saved <- readLines("last_id.txt")
 
 retweet_nflverse <- function(last_id_saved){
 
-  tweets_raw <- search_tweets(q = "#nflverse", n = 18000)
+  tweets_raw <- search_tweets(
+    q = "#nflverse",
+    n = 1000,
+    include_rts = FALSE,
+    since_id = last_id_saved,
+    retryonratelimit = TRUE
+  )
 
   tweets_filtered <- tweets_raw %>%
-    filter(!is_retweet,
-           as.numeric(status_id) > as.numeric(last_id_saved),
-           map_lgl(hashtags, ~length(.x) <= 3)
-    ) %>%
-    group_by(user_id) %>%
-    slice_min(order_by = status_id, n = 3) %>%
-    ungroup()
+    tidyr::unnest_wider(entities) |>
+    dplyr::filter(!is.na(id)) |>
+    dplyr::filter(purrr::map_lgl(hashtags, ~nrow(.x) <= 3))
 
-  writeLines(max(tweets_raw$status_id), "last_id.txt")
+  writeLines(max(tweets_raw$id), "last_id.txt")
 
-  if(nrow(tweets_filtered)>0) return("No tweets to retweet")
+  if(!nrow(tweets_filtered)>0) return("No tweets to retweet")
 
-  r <- map(x,~post_tweet(retweet_id = .x))
+  r <- purrr::map(tweets_filtered$id_str,~rtweet::post_tweet(retweet_id = .x))
 
   m <- paste(nrow(tweets_filtered)," #nflverse messages retweeted!")
 
