049406e0a0c9340c1753562c85d9fd6dda58eb38
mrcaseb <roemerle@gmail.com>
2021-10-03 20:55:55 +0200

    add stuff for nflverse plot tweet

--- build_hashtag_plot.R
+++ build_hashtag_plot.R
@@ -1,34 +1,57 @@
-Sys.setlocale("LC_ALL","English")
-nflversetweets <- rtweet::search_tweets("#nflverse", n = 18000, include_rts = FALSE)
+suppressPackageStartupMessages({
+  library(rtweet)
+  library(dplyr, warn.conflicts = FALSE)
+  library(ggplot2, warn.conflicts = FALSE)
+})
 
-stop <- lubridate::today("America/New_York") - 1
-start <- lubridate::today("America/New_York") - 7
+api_key <- Sys.getenv("TWITTERAPIKEY")
+api_secret <- Sys.getenv("TWITTERAPISECRET")
+access_token <- Sys.getenv("TWITTERACCESSTOKEN")
+access_secret <- Sys.getenv("TWITTERACCESSTOKENSECRET")
+bot <- rtweet::rtweet_bot(
+  api_key = api_key,
+  api_secret = api_secret,
+  access_token = access_token,
+  access_secret = access_secret
+)
 
-library(ggplot2)
-dummy <- data.frame(post_day = 1:7, n = 0)
+rtweet::auth_as(bot)
+
+nflversetweets <- rtweet::search_tweets("#nflverse",
+                                        n = 1000,
+                                        include_rts = FALSE,
+                                        retryonratelimit = TRUE)
+
+# last Monday
+start <- lubridate::floor_date(lubridate::today("America/New_York") - 1, "week", 1)
+# latest Sunday
+stop <-  lubridate::floor_date(lubridate::today("America/New_York"), "week", 7)
+all_days <- tibble::tibble(created_at = lubridate::as_date(start:stop))
+
 last_week <- nflversetweets |>
+  dplyr::filter(!is.na(created_at)) |>
+  dplyr::mutate(
+    created_at = glue::glue("{stringr::str_sub(created_at, -4)}-{stringr::str_sub(created_at, 5,7)}-{stringr::str_sub(created_at, 9,10)}"),
+    created_at = lubridate::as_date(created_at)
+  ) |>
   dplyr::filter(created_at < stop) |>
   dplyr::filter(created_at > start) |>
-  dplyr::transmute(post_day = lubridate::wday(created_at, label = FALSE, week_start = 4)) |>
+  dplyr::bind_rows(all_days) |>
+  dplyr::transmute(post_day = lubridate::wday(created_at, label = TRUE, week_start = 1)) |>
   dplyr::count(post_day) |>
-  dplyr::bind_rows(dummy) |>
-  dplyr::group_by(post_day) |>
-  dplyr::slice_max(n, n = 1) |>
-  dplyr::ungroup() |>
-  dplyr::mutate(post_day_word = c("Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"))
+  dplyr::mutate(n = n - 1L)
 
-last_week |>
-  ggplot2::ggplot(aes(x = forcats::fct_reorder(post_day_word, post_day), y = n)) +
+plot <- last_week |>
+  ggplot2::ggplot(aes(x = post_day, y = n)) +
   ggplot2::geom_col(width = 0.75, color = "white", fill = "#1B95E0") +
-  # ggplot2::geom_label(y_nudge = 1, aes(label = n)) +
+  ggplot2::geom_text(aes(label = paste0(n, "\n")), vjust = 0, color = "white", fontface = "bold", lineheight = ggplot2::unit(0.2, "lines")) +
   ggplot2::labs(
     title = "Daily Number of Tweets using the <span style = 'color:#1B95E0;'>#nflverse</span> Hashtag",
-    subtitle = glue::glue("Total of {sum(last_week$n)} tweets between {start} and {stop}"),
+    subtitle = glue::glue("Total of {sum(last_week$n)} Tweets Between {format(start, '%b %d, %Y')} and {format(stop, '%b %d, %Y')}"),
     y = "Number of Tweets",
     caption = "Data: rtweet | Plot: @mrcaseb"
   ) +
   ggplot2::scale_y_continuous(breaks = scales::breaks_pretty()) +
-  # ggplot2::scale_x_discrete(breaks = 1:7, labels = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")) +
   hrbrthemes::theme_modern_rc(plot_margin = ggplot2::margin(10, 10, 10, 10), grid = "Y") +
   ggplot2::theme(
     axis.title.x = ggplot2::element_blank(),
@@ -36,4 +59,10 @@ last_week |>
     plot.title = ggtext::element_markdown()
   )
 
-ggsave("plot.png", width = 15, height = 10, units = "cm", dpi = 600)
+ggplot2::ggsave("plot.png", width = 17, height = 12, units = "cm", dpi = 600)
+
+rtweet::post_tweet(
+  status = "Number of nflverse tweets over the last week, posted through #rstats courtesy of #rtweet!",
+  media = "plot.png",
+  media_alt_text = "Number of tweets using the #nflverse hashtag."
+)
